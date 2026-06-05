679708ff583a15c9c5b0099df17e8126bf197b45
ChifundoGKauwa <kauwachifundo114@gmail.com>
2026-06-05 21:27:18 +0200

    fix(build): mark login and signup as force-dynamic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 import Login from "@/src/components/login";
 
+export const dynamic = "force-dynamic";
+
 export default function LoginPage() {
   return <Login />;
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,7 @@
 import SignUp from "@/src/components/signup";
 
+export const dynamic = "force-dynamic";
+
 export default function SignUpPage() {
   return <SignUp />;
 }
