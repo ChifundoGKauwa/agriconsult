1c19699f79526196df52147cd729155dd82d16b6
ChifundoGKauwa <kauwachifundo114@gmail.com>
2026-06-05 18:12:01 +0200

    fix(build): mark consulting page as force-dynamic

--- app/consulting/page.tsx
+++ app/consulting/page.tsx
@@ -1,5 +1,7 @@
 import ConsultingPage from "@/src/components/consulting/ConsultingPage";
 
+export const dynamic = "force-dynamic";
+
 export default function ConsultingRoute() {
   return <ConsultingPage />;
 }
