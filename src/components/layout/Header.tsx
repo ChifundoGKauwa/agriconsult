727d9a60f8d5e0d995b8afc0475bd50c0478ca04
ChifundoGKauwa <kauwachifundo114@gmail.com>
2026-06-05 09:16:05 +0200

    feat(ui): wire Schedule Consultation button to consulting page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -24,9 +24,11 @@ export default function Header() {
           <a className="hover:text-primary" href="/login">
             Login
           </a>
-          <Button size="sm" className="bg-accent text-primary hover:bg-tertiary">
-            Schedule Consultation
-          </Button>
+          <a href="/consulting">
+            <Button size="sm" className="bg-accent text-primary hover:bg-tertiary">
+              Schedule Consultation
+            </Button>
+          </a>
         </div>
       </Container>
     </header>
