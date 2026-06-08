d65e257eac6c942a309517015e6a16cf6f915817
ChifundoGKauwa <kauwachifundo114@gmail.com>
2026-06-08 09:41:57 +0200

    Remove upload advertisement button from advertising page

--- src/components/advertising/AdvertisingPage.tsx
+++ src/components/advertising/AdvertisingPage.tsx
@@ -190,22 +190,7 @@ export default function AdvertisingPage() {
                   Sign in to upload
                 </Button>
               </a>
-            ) : (
-              <Button
-                type="button"
-                size="sm"
-                onClick={() => setShowUploadModal(true)}
-                disabled={isSaving}
-                className="bg-accent text-primary hover:bg-tertiary"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                  <polyline points="17 8 12 3 7 8" />
-                  <line x1="12" y1="3" x2="12" y2="15" />
-                </svg>
-                {isSaving ? "Saving..." : "Upload advertisement"}
-              </Button>
-            )}
+            ) : null}
           </div>
 
           {/* Filter Bar */}
@@ -398,14 +383,7 @@ export default function AdvertisingPage() {
                   >
                     Cancel
                   </Button>
-                  <Button
-                    type="button"
-                    onClick={handleSaveAd}
-                    disabled={isSaving || !formTitle}
-                    className="bg-primary text-white hover:bg-primary/90"
-                  >
-                    {isSaving ? "Saving..." : "Publish Advertisement"}
-                  </Button>
+                 
                 </div>
               </div>
             )}
