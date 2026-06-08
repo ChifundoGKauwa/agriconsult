e25ba88c0fdcfd4d569fa5b2352528371a0aec1d
ChifundoGKauwa <kauwachifundo114@gmail.com>
2026-06-08 09:56:39 +0200

    Remove all upload/sign-in buttons from advertising page

--- src/components/advertising/AdvertisingPage.tsx
+++ src/components/advertising/AdvertisingPage.tsx
@@ -171,26 +171,7 @@ export default function AdvertisingPage() {
                 Top-tier agricultural products vetted by AgriConsult experts.
               </p>
             </div>
-            {!uploadPreset ? (
-              <Button variant="ghost" disabled>
-                Upload preset missing
-              </Button>
-            ) : !user ? (
-              <a href="/login">
-                <Button
-                  type="button"
-                  size="sm"
-                  className="bg-accent text-primary hover:bg-tertiary"
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                    <polyline points="17 8 12 3 7 8" />
-                    <line x1="12" y1="3" x2="12" y2="15" />
-                  </svg>
-                  Sign in to upload
-                </Button>
-              </a>
-            ) : null}
+            {null}
           </div>
 
           {/* Filter Bar */}
